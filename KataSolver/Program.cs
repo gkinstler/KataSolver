c1918c6f2455e8ef1a759bda3ef804c13e226f3c
Glenn Kinstler <glenn@glennkinstler.com>
2018-10-03 13:14:07 -0500

    Add project files.

---
+++ KataSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
